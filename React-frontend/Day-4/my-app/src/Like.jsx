356462ea9f851eb41a92927796f825095d9605b3
Priya <priyamohana325@gmail.com>
2023-08-11 18:19:17 +0530

    react_commit

---
+++ React-frontend/Day-4/my-app/src/Like.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import fc from './fc'
+
+function Like() {
+  return (
+    <>
+    <button>Like</button>
+    <fc></fc>
+    </>
+  )
+}
+
+export default Like
---
+++ React-frontend/Day-4/my-app/src/fc.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function fc() {
+  return (
+    <button>cmtt</button>
+  )
+}
+
+export default fc
